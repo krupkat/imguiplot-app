e493920688a142e06427a8a06eb886afa4946bcd
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2022-12-04 21:35:51 +0100

    simplify glfw app init

--- src/glfw_opengl3_app.cpp
+++ src/glfw_opengl3_app.cpp
@@ -36,21 +36,10 @@ bool GlfwOpenGL3Backend::Init() {
     return false;
   }
 
-#if defined(__APPLE__)
-  // GL 3.2 + GLSL 150
-  const char* glsl_version = "#version 150";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
-#else
-  // GL 3.0 + GLSL 130
-  const char* glsl_version = "#version 130";
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+
-  // only glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
-#endif
 
   window_ = glfwCreateWindow(options_.window_width, options_.window_height,
                              options_.window_title.c_str(), nullptr, nullptr);
@@ -66,7 +55,7 @@ bool GlfwOpenGL3Backend::Init() {
   ImPlot::CreateContext();
 
   ImGui_ImplGlfw_InitForOpenGL(window_, true);
-  ImGui_ImplOpenGL3_Init(glsl_version);
+  ImGui_ImplOpenGL3_Init();
 
   bg_color_ = ToFloat(options_.bg_color);
 
